e5e55f56fd3495b3166ac89d7a66fda2b9713a8f
cagum <mochammad.cahya_ti22@nusaputra.ac.id>
2024-03-31 16:53:02 +0700

    Generic_Collection MochCahyaG_PBO_s7

---
+++ GenLib.java
@@ -0,0 +1,8 @@
+public class GenLib {
+    public static <T extends Number, X extends Number> void add(T a, X b) {
+    }
+
+    public static <A,B> void printAlamat(A a, B b) {
+        System.out.println(a + " " + b);
+    }
+}
